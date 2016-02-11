c0215ee90c4f8079c7ad8cd5b9a5cad8f3c882f9
danik <me@subsonic.cz>
2016-02-11 23:22:52 +0100

    hell yeah babe

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 BASE_DIR=$(dirname $(dirname $(realpath $0)))
-COMPOSER_HOME="$BASE_DIR/temp/.composer"
+
+export COMPOSER_HOME="$BASE_DIR/temp/.composer"
 
 function deploy {
     set -e
