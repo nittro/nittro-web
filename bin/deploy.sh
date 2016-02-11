fc908429c8d91fd6fdb1a02e587f8859f598a0ba
danik <me@subsonic.cz>
2016-02-11 23:26:56 +0100

    hell yeah babe

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 BASE_DIR=$(dirname $(dirname $(realpath $0)))
+set -e
 
-function deploy {
-    set -e
-
-    rm -rf temp/cache/*
-
-    git reset --hard
-    git pull
-    COMPOSER_HOME="$BASE_DIR/.composer" composer install
-    bower install
-    npm install
-    ./node_modules/.bin/grunt
-
-}
 
 cd "$BASE_DIR"
 
 cp -f www/.maintenance.php www/maintenance.php
 
-deploy
+rm -rf temp/cache/*
+git reset --hard
+git pull
+COMPOSER_HOME="$BASE_DIR/.composer" composer install
+bower install
+npm install
+./node_modules/.bin/grunt
 
 rm www/maintenance.php
