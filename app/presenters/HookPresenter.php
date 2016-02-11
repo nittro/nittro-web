8cb646272c1f3a7929f41f6e444177c171c712f1
danik <me@subsonic.cz>
2016-02-11 17:41:32 +0100

    init commit

--- app/presenters/HookPresenter.php
+++ app/presenters/HookPresenter.php
@@ -9,13 +9,32 @@
 namespace App\Presenters;
 
 
+use Nette\Neon\Neon;
+
 class HookPresenter extends BasePresenter {
 
     public function actionDefault() {
         $reposPath = $this->context->parameters['reposPath'];
-        $payload = $this->getHttpRequest()->getPost();
 
-        file_put_contents($reposPath . '/hook.json', json_encode($payload));
+        $event = $this->getHttpRequest()->getHeader('X-Github-Event');
+
+        switch ($event) {
+            case 'gollum':
+                exec('cd ' . escapeshellarg($reposPath . '/wiki') . ' && git pull');
+                break;
+
+            case 'release':
+                $reposPath = escapeshellarg($reposPath . '/nettejs.git');
+                exec('cd ' . $reposPath . ' && git fetch');
+                $latest = trim(`cd $reposPath && git tag --sort=-v:refname | head -n 1`);
+                file_put_contents($this->context->parameters['appDir'] . '/config/repo-version.neon', Neon::encode(['parameters' => ['netteJsVersion' => $latest]]));
+                break;
+
+            case 'push':
+                $path = $this->context->parameters['appDir'] . '/..';
+                exec('cd ' . escapeshellarg($path) . ' && git pull && composer install');
+                break;
+        }
 
         $this->terminate();
 
