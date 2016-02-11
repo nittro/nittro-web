c4ff9870dcde458ea6ed74d91db5c8f1720848ce
danik <me@subsonic.cz>
2016-02-11 23:08:55 +0100

    hell yeah babe

--- www/.maintenance.php
+++ www/.maintenance.php
@@ -10,7 +10,8 @@ header('Retry-After: 300'); // 5 minutes in seconds
 <meta name="generator" content="Nette Framework">
 
 <style>
-	body { color: #333; background: white; width: 500px; margin: 100px auto }
+    html { background: #312450; }
+	body { color: rgba(255,255,255,0.55); background: #312450; width: 500px; margin: 100px auto }
 	h1 { font: bold 47px/1.5 sans-serif; margin: .6em 0 }
 	p { font: 21px/1.5 Georgia,serif; margin: 1.5em 0 }
 </style>
