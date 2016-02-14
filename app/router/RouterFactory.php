1b71a2cf25687f05e3bbe32fb3a9b088e86096e8
danik <me@subsonic.cz>
2016-02-14 23:19:36 +0100

    fix redirect loop

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -13,12 +13,15 @@ class RouterFactory
 	/**
 	 * @return Nette\Application\IRouter
 	 */
-	public static function createRouter()
+	public static function createRouter($productionMode)
 	{
+        $flag = $productionMode ? Route::SECURED : 0;
+
 		$router = new RouteList;
-		$router[] = new Route('wiki[/<path .+>]', 'Wiki:default');
-		$router[] = new Route('hook', 'Hook:default');
-		$router[] = new Route('<action>[/<step \d+>]', 'Homepage:default');
+        $router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY | $flag);
+		$router[] = new Route('wiki[/<path .+>]', 'Wiki:default', $flag);
+		$router[] = new Route('hook', 'Hook:default', $flag);
+		$router[] = new Route('<action>[/<step \d+>]', 'Homepage:default', $flag);
 		return $router;
 	}
 
