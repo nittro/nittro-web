41d0f5a34b2c5f340475312452f5608f1efca49c
danik <me@subsonic.cz>
2016-02-15 05:59:10 +0100

    some facelifts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,11 +9,11 @@ cd "$BASE_DIR"
 cp -f www/.maintenance.php www/maintenance.php
 
 rm -rf temp/cache/*
-git reset --hard
-git pull
-COMPOSER_HOME="$BASE_DIR/.home/composer" composer install
+HOME="$BASE_DIR/.home" git reset --hard
+HOME="$BASE_DIR/.home" git pull
+HOME="$BASE_DIR/.home" composer install
 HOME="$BASE_DIR/.home" bower install
-npm install
-./node_modules/.bin/grunt
+HOME="$BASE_DIR/.home" npm install
+HOME="$BASE_DIR/.home" ./node_modules/.bin/grunt
 
 rm www/maintenance.php
