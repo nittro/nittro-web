f5a253631871350f5f5dfce7efc5f5746c5382be
danik <me@subsonic.cz>
2016-03-01 00:09:37 +0100

    fix error presenter

--- app/presenters/Error4xxPresenter.php
+++ app/presenters/Error4xxPresenter.php
@@ -21,6 +21,7 @@ class Error4xxPresenter extends Nette\Application\UI\Presenter
 	{
 		// load template 403.latte or 404.latte or ... 4xx.latte
 		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
+        $this->template->tab = null;
 		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
 	}
 
